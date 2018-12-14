c6a5d37be1ef2db6c2c5e6615df4adca60c566ca
Diego Henriquez <df.henriquez@hotmail.com>
2018-12-14 16:43:01 -0600

    Initial Commit and Startup basic config

---
+++ netcorewebapi/Controllers/ValueController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace netcorewebapi.Controllers
+{
+    [Route("api/[controller]")]
+    
+    public class ValueController : ControllerBase
+    {
+        public ValueController()
+        {
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string [] {"Value1", "Value2", "Value3"});
+        }
+
+    }
+}
