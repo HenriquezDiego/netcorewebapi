d6dacab76455bd6505d9962f16db342c396bcbfb
Diego Henriquez <df.henriquez@hotmail.com>
2019-08-30 14:59:09 -0500

    CoreController creado con el fin de reutilizar código (beta)

---
+++ src/Netcorewebapi.Api/Controllers/CoreController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NetcorewebApi.Api.Controllers
+{
+    [ApiController]
+    public class CoreController<TEntity,TParameters> : ControllerBase
+    {
+        
+        [HttpGet]
+        public virtual IActionResult Get(TParameters parameters)
+        {
+            return null;
+        }
+
+        [HttpGet("{id}")]
+        public virtual IActionResult Get(int id) 
+        {
+            return null;
+        }
+
+        [HttpGet(template:"/GetAsync")]
+        public virtual Task<IActionResult> GetAsync(TParameters parameters)
+        {
+            return null;
+        }
+
+        [HttpGet("/GetAsync/{id}")]
+        public virtual Task<IActionResult> GetAsync(int id) 
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public virtual IActionResult Post(TEntity model)
+        {
+            return null;
+        }
+
+        [HttpPut("{id}")]
+        public virtual IActionResult Put(int id, TEntity model)
+        {
+            return null;
+        }
+
+        [HttpDelete("{id}")]
+        public virtual IActionResult Delete(int id)
+        {
+            
+            return null;
+            
+        }
+
+    }
+}
---
+++ src/Netcorewebapi.Api/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Netcorewebapi.Common.Helpers;
+using Netcorewebapi.DataAccess.Entities;
+using NetcorewebApi.DataAccess.Persistence.Repositories;
+
+namespace NetcorewebApi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : CoreController<Product,ProductParameters>
+    {
+        private readonly IProductRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly IUrlHelper _urlHelper;
+
+        public TestController(IProductRepository repository, 
+            IMapper mapper,
+            IUrlHelper iUrlHelper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _urlHelper = iUrlHelper;
+        }
+
+        
+    }
+}
